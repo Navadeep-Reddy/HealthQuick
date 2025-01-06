cb7bdca7d3459bb759e434b5d47844097f15d3ab
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-01-06 23:44:01 +0530

    Created API to retrieve user _id based on google id from cloud database

--- server/src/controller.js
+++ server/src/controller.js
@@ -2,7 +2,34 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI("AIzaSyAEgDO5u4HX2vHsOFrepxChz6WhUKmTd5w");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const getRecommendFood = async (req, res) => {
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Navadeep:ObkEeEdXvdNfEqlh@userdata.4xm3b.mongodb.net/?retryWrites=true&w=majority&appName=UserData";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const db = client.db('hquickie');
+const user_collection = db.collection('users');  
+
+
+// test function to show how mongoDB can be used here
+async function test() {
+
+    const query = {name: "Navadeep"};
+
+    const user = await user_collection.findOne(query);
+
+    console.log(user);
+}
+//test()
+
+const postRecommendFood = async (req, res) => {
   try {
     const { prompt } = req.body;
     
@@ -10,7 +37,7 @@ const getRecommendFood = async (req, res) => {
       return res.status(400).json({ error: "Prompt is required" });
     }
 
-    const structuredPrompt = `Return ONLY a raw JSON object (no markdown, no backticks) with nutritional information for ${prompt} with this exact structure an ddnt mention the units:
+    const structuredPrompt = `Return ONLY a raw JSON object (no markdown, no backticks) with nutritional information for ${prompt} with this exact structure an do not mention the units:
     {
       "Proteins": "<value> g",
       "Carbs": "<value> g",
@@ -34,6 +61,18 @@ const getRecommendFood = async (req, res) => {
   }
 };
 
+const getUserId = async (req, res) => {
+    const g_id = req.params.googleid;
+
+    // Directing writing the query along with the options which will have sort and projection
+    const obj_id = await user_collection.findOne({googleId: g_id}, {
+        projection: {_id:true}
+    })
+
+    res.json(obj_id)
+}
+
 module.exports = {
-  getRecommendFood
+  postRecommendFood,
+  getUserId
 }
--- server/src/router.js
+++ server/src/router.js
@@ -2,9 +2,9 @@ const { Router } = require("express");
 const router = Router();
 const controller = require('./controller');
 
-router.post('/sampleAPI', controller.getRecommendFood)
+router.post('/recom', controller.postRecommendFood)
 
-
+router.get('/userid:googleid', controller.getUserId)
 
 
 module.exports = router;
