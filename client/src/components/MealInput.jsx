d8d9993283efbc7811c6bd9395b5a2553f14e9d3
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-01-08 23:26:25 +0530

    Added button for meal

--- client/src/components/MealInput.jsx
+++ client/src/components/MealInput.jsx
@@ -7,8 +7,9 @@ const MealInput = () => {
   const [mealsList, setMeal] = useState([]);
 
   return (
-    <div className='bg-red-400 w-full mt-5'>
-        <CiCirclePlus className='text-3xl' />
+    <div className=' w-[50%] mt-[60px] flex items-center justify-center'>
+        <p className='mx-2 text-2xl text-TBlack'>Add meal</p>
+        <CiCirclePlus className='text-5xl mx-2 text-DGreen' />
     </div>
   )
 }
